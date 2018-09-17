1ff631245da6d8411e34e1d10749e2592d222743
Jonáš Svatoš <jonas.svatos@nfa.cz>
2018-09-17 08:53:00 +0000

    Update app.py

--- app.py
+++ app.py
@@ -21,6 +21,9 @@ app = Flask(__name__)
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def root(path):
+    if 'Safari' in request.headers.get('User-Agent'):
+        return('Apple Safari is not supported, please use Mozilla Firefox or Google Chrome')
+
     auth_data = {'client_id': args.client_id,
                 'client_secret': args.client_secret,
                 'grant_type': 'password',
