58a87bdd6b96bd6ef5f7f0c40429b04c35550556
Jonas Svatos <jonas.svatos@nfa.cz>
2019-06-08 10:29:04 +0200

    catch any registration errors before redirecting
    add ability to automatically disable webtorrent in user settings, refactor a bit

--- wsgi.py
+++ wsgi.py
@@ -15,6 +15,7 @@ parser.add_argument('-s', '--client_secret', help='Client secret to use', defaul
 parser.add_argument('-e', '--endpoint', help='Peertube endpoint', default=os.getenv('PEERTUBE_ENDPOINT', 'http://localhost:9000'))
 parser.add_argument('-H', '--host', help='Host to listen on', default=os.getenv('LISTEN_HOST', 'localhost'))
 parser.add_argument('-P', '--port', help='Port', default=os.getenv('LISTEN_PORT', '9001'))
+parser.add_argument('--nowebtorrent', help='Disable webTorrent', default=bool(os.getenv('PEERTUBE_DISABLE_WEBTORRENT', 'False')))
 args = parser.parse_args()
 application = Flask(__name__)
 
@@ -33,22 +34,30 @@ def root(path):
                 'username': request.headers.get('X-User').lower() if 'X-User' in request.headers else args.username
                 }
     auth_result = requests.post('{0}{1}'.format(args.endpoint, '/api/v1/users/token'), data=auth_data)
-    try:
-        data = auth_result.json()
-    except:
-        return(auth_result.text, 500)
-
-    if data.get('code') == 'invalid_grant':
+    if auth_result.status_code == 400 and 'invalid_grant' in auth_result:
         registration_data = {'email': request.headers.get('X-Email') if 'X-Email' in request.headers else '{0}@nfa.cz'.format(args.username),
                              'password': args.password,
                              'terms': 'true',
                              'username': request.headers.get('X-User').lower() if 'X-User' in request.headers else args.username}
         user_registration = requests.post('{0}{1}'.format(args.endpoint, '/api/v1/users/register'), data=registration_data)
-        return(redirect('/{0}'.format(path)))
+        if not user_registration.status_code == 204:
+            return(user_registration.text, user_registration.status_code)
+        else:
+            return(redirect('/{0}'.format(path)))
+    elif not auth_result.status_code == 200:
+        return(auth_result.text, auth_result.status_code)
+    else:
+        data = auth_result.json()
 
     try:
         access_token = (data['access_token'])
-        user_info = requests.get('{0}{1}'.format(args.endpoint, '/api/v1/users/me'), headers={'Authorization': 'Bearer {0}'.format(access_token)}).json()
+        auth_headers = {'Authorization': 'Bearer {0}'.format(access_token)}
+        user_info = requests.get('{0}{1}'.format(args.endpoint, '/api/v1/users/me'), headers=auth_headers).json()
+
+        if args.nowebtorrent and user_info['webTorrentEnabled'] == True:
+            user_modify = requests.put('{0}{1}'.format(args.endpoint, '/api/v1/users/me'), headers=auth_headers, data={'webTorrentEnabled': "false"})
+            if not user_modify.status_code == 204: return(user_modify.text, user_modify.status_code)
+
         refresh_token = (data['refresh_token'])
         token_type = (data['token_type'])
         local_storage_data = ['localStorage.setItem("access_token", "{0}");'.format(access_token),
